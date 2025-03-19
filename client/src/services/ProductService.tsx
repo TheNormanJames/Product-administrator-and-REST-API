b44b0b3e097cfe6c907939858d6ecccea0a87c0e
TheNormanJames <normane_@hotmail.com>
2025-03-19 16:45:13 -0500

    Agregando Rutas de Frontend

---
+++ client/src/services/ProductService.tsx
@@ -0,0 +1,18 @@
+import { safeParse } from "valibot";
+import { DraftProductSchema } from "../types";
+
+type ProductData = {
+  [k: string]: FormDataEntryValue;
+};
+
+export function addProduct(data: ProductData) {
+  try {
+    const result = safeParse(DraftProductSchema, {
+      name: data.name,
+      price: +data.price,
+    });
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
