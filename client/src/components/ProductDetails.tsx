9bf06640737b773d143b0501ec186751a3f404e1
NormanJaimes <normane_@hotmail.com>
2025-03-24 18:02:08 -0500

    feat: :sparkles: flow to edit product, show url and values. lesft to update

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { Product } from '../types';
 import { formatCurrency } from '../utils';
 
@@ -6,6 +7,7 @@ type ProductDetailsProps = {
 };
 
 export default function ProductDetails({ product }: ProductDetailsProps) {
+  const navigate = useNavigate();
   const isAvailbility = product.availability;
   return (
     <tr className="border-b ">
@@ -16,7 +18,16 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
       <td className="p-3 text-lg text-gray-800">
         {isAvailbility ? 'Disponible' : 'No Disponible'}
       </td>
-      <td className="p-3 text-lg text-gray-800 ">{product.name}</td>
+      <td className="p-3 text-lg text-gray-800 ">
+        <div className="flex gap-2 items-center">
+          <button
+            className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center cursor-pointer"
+            onClick={() => navigate(`/productos/${product.id}/editar`)}
+          >
+            Editar
+          </button>
+        </div>
+      </td>
     </tr>
   );
 }
--- client/src/services/ProductService.tsx
+++ client/src/services/ProductService.tsx
@@ -1,5 +1,10 @@
 import { safeParse } from 'valibot';
-import { DraftProductSchema, ProductsSchema } from '../types';
+import {
+  DraftProductSchema,
+  Product,
+  ProductSchema,
+  ProductsSchema,
+} from '../types';
 import axios from 'axios';
 
 type ProductData = {
@@ -35,6 +40,20 @@ export async function getProducts() {
     const url = `${import.meta.env.VITE_API_URL}/api/products`;
     const { data } = await axios(url);
     const result = safeParse(ProductsSchema, data.data);
+    if (result.success) {
+      return result.output;
+    } else {
+      throw new Error('Hubo un error En el safeParse');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function getProductById(id: Product['id']) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+    const { data } = await axios(url);
+    const result = safeParse(ProductSchema, data.data);
     if (result.success) {
       return result.output;
     } else {
